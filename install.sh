ef513161f224dcf3fe367dbd8ac7795e895a218f
Micky Hulse <mickyhulse@gmail.com>
2016-03-27 13:33:38 -0700

    Fixing #4

--- install.sh
+++ install.sh
@@ -14,7 +14,7 @@ function install() {
 	SOURCE_ZIP="https://github.com/mhulse/bootgeoisie/tarball/master"
 	
 	# Get the zip file and extract all files:
-	curl -sS -#L "$SOURCE_ZIP" | tar -xzv --strip-components 1 --exclude={install.sh,README.md}
+	curl -sS -#L "$SOURCE_ZIP" | tar -xzv --strip-components 1 --exclude={install.sh}
 	
 	# Let the use know that we are done:
 	echo $'\n'"Congrats! Installation was successful!"$'\n'
